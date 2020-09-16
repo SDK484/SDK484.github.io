4ff13e176379261f4b62b298e6d2df4e0090b54b
SDK484 <stevendkennedy91@gmail.com>
2020-09-16 17:17:09 +0100

    Update camera.

--- OtherPrototypes/Camera/js/main.js
+++ OtherPrototypes/Camera/js/main.js
@@ -38,9 +38,14 @@ document.getElementById("flip4").addEventListener("click", function() {
 
 
 // Get access to the camera!
+const constraints = {
+  video: {
+    facingMode: "environment"
+  }
+};
 if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
 	// Not adding `{ audio: true }` since we only want video now
-	navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+	navigator.mediaDevices.getUserMedia({ video: constraints }).then(function(stream) {
 			//video.src = window.URL.createObjectURL(stream);
 			video.srcObject = stream;
 			video.play();
